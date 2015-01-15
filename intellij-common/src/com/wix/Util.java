202feae57c8527b39bb6e52901578074375107c6
ido <idok@wix.com>
2015-01-15 14:17:46 +0200

    make static

--- intellij-common/src/com/wix/Util.java
+++ intellij-common/src/com/wix/Util.java
@@ -3,7 +3,9 @@ package com.wix;
 /**
  * Created by idok on 8/12/14.
  */
-public class Util {
+public final class Util {
+    private Util() {
+    }
 
     public static final String LOG_ID = "#com.wix.common";
 
